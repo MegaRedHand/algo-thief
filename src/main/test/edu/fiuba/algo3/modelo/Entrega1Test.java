4f7d58397ed18cce673ae4620779eca728552cdf
Tomas Gruner <tgruner@fi.uba.ar>
2021-12-03 12:46:30 -0300

    chore: agrego estructura para los tests

---
+++ src/main/test/edu/fiuba/algo3/modelo/Entrega1Test.java
@@ -0,0 +1,60 @@
+package edu.fiuba.algo3.modelo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Entrega1Test {
+//    /**Caso de uso 1
+//     * - Robaron el tesoro Nacional de Montreal.
+//     * - Sospechoso femenino.
+//     * - Detective novato comienza en Montreal.
+//     * - Visita un Banco:
+//     *   - Se despliega una pista.
+//     */
+//    @Test
+//    public void detectiveNovatoEmpiezaEnMontrealYAlVisitarBancoDespliegaUnaPista() {
+//        assertEquals(1, 1);
+//    }
+
+//    /**Caso de uso 2
+//     * - Detective novato comienza en Montreal.
+//     * - Visita un Banco (nuevamente):
+//     *   - Se despliega una pista
+//     * - Visita una Biblioteca:
+//     *   -Se despliega una pista.
+//     */
+//     @Test
+//    public void detectiveNovatoEmpiezaEnMontrealYAlVisitarVariosEdificiosDespliegaVariasPistas() {
+//         assertEquals(1, 1);
+//    }
+
+//    /**Caso de uso 3
+//     * - Detective viaja de Montreal a México
+//     */
+//    @Test
+//    public void detectiveViajaDeMontrealAMexico() {
+//        assertEquals(1, 1);
+//    }
+
+//    /**Caso de uso 4
+//     * - Visita un Aeropuerto (3 veces):
+//     *   - Se despliega una pista.
+//     * - Visita un Puerto (55 veces):
+//     *   - Se despliega una pista.
+//     */
+//    @Test
+//    public void detectiveAlVisitaAeropuertoSeDespliegaPistaYAlVisitarPuertoSeDespliegaPista() {
+//        assertEquals(1, 1);
+//    }
+
+//    /**Caso de uso 5
+//     * - Detective sufre una herida de cuchillo.
+//     * - Detective duerme.
+//     */
+//    @Test
+//    public void detectiveSufreHeridaDeCuchilloYDuerme() {
+//        assertEquals(1, 1);
+//    }
+
+}
