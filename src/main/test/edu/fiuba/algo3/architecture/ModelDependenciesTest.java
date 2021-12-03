79cbb3b49036cd64be2d6b344dc9df9f954a874a
Tomas Gruner <tgruner@fi.uba.ar>
2021-12-03 13:31:07 -0300

    fix: agrego Mockito a ModelDependenciesTest
    Supongo que sí, pero pregunté por Slack si deberíamos usarlo.
    Link a la conversación: https://programacion3fiuba.slack.com/archives/C0173GAK69L/p1638548483041200?thread_ts=1638370087.035900&cid=C0173GAK69L

--- src/main/test/edu/fiuba/algo3/architecture/ModelDependenciesTest.java
+++ src/main/test/edu/fiuba/algo3/architecture/ModelDependenciesTest.java
@@ -7,20 +7,22 @@ import org.junit.jupiter.api.Test;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
+@SuppressWarnings("ALL")
 public class ModelDependenciesTest {
     private final String MODEL = "..modelo..";
     private final String JAVA_LANG = "java..";
     private final String JUNIT = "org.junit..";
+    private final String MOCKITO = "org.mockito..";
 
     @Test
     public void elModeloSoloPuedeReferenciarClasesDelModeloAdemasJavaAdemasJunit() {
         JavaClasses importedClasses = new ClassFileImporter().importPackages("edu.fiuba.algo3.modelo");
 
-        String[] listOfPackages = {MODEL, JAVA_LANG, JUNIT};
+        String[] listOfPackages = {MODEL, JAVA_LANG, JUNIT, MOCKITO};
 
         ArchRule myRule = classes().that().resideInAPackage(MODEL)
                 .should().onlyDependOnClassesThat().resideInAnyPackage(listOfPackages);
-
+        // http://github.com/pkrumins/lulzbot/commit/4c1b1ea95b379805e3b595b511a89affc6811145
         myRule.check(importedClasses);
     }
 
