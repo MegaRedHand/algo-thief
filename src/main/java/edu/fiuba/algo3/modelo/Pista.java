54adadf769f1180e36c958ae9e1c4019773f98f5
Tomás <47506558+MegaRedHand@users.noreply.github.com>
2021-12-06 13:21:23 -0300

    Caso de uso 1 (#1)
    * fix: agrego Mockito a ModelDependenciesTest
    Supongo que sí, pero pregunté por Slack si deberíamos usarlo.
    Link a la conversación: https://programacion3fiuba.slack.com/archives/C0173GAK69L/p1638548483041200?thread_ts=1638370087.035900&cid=C0173GAK69L
    * feat: implemento solución del caso de uso 1
    * Tests
    * Test 01 pass
    * Test 01 y 02 pass
    * Test 01 y 02 pass
    * Test 03 y 04 pass
    * Agrego varargs al constructor de Ciudad
    * feat: implemento test05
    * agrego el cronómetro y sus chequeos a los tests anteriores
    * agrego funcionalidad de tiempo de viaje al rango Novato
    * agrego pruebas unitarias para Novato
    * cambio Rango a interfaz
    
    Co-authored-by: Santiago Vaccarelli <svaccarelli@fi.uba.ar>
    Co-authored-by: Lucia <llourengo@fi.uba.ar>

---
+++ src/main/java/edu/fiuba/algo3/modelo/Pista.java
@@ -0,0 +1,10 @@
+package edu.fiuba.algo3.modelo;
+
+public class Pista {
+
+    private final String descripcion;
+
+    public Pista(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
