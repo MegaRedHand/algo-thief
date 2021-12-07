fcc7a41fb09e13e264ce2f97787bebea849e55be
Tomas Gruner <tgruner@fi.uba.ar>
2021-12-06 21:49:21 -0300

    chore: actualizo diagramas de clase

--- src/main/java/edu/fiuba/algo3/modelo/Comun.java
+++ src/main/java/edu/fiuba/algo3/modelo/Comun.java
@@ -3,7 +3,7 @@ package edu.fiuba.algo3.modelo;
 public class Comun {
     private final String nombre;
 
-    public Comun (String nombre) {
+    public Comun(String nombre) {
         this.nombre = nombre;
     }
 }
