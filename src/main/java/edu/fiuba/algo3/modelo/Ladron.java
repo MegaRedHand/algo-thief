04ad2ff791dcec6db0ad2c4c6d11c82a106fb86c
Tomas Gruner <tgruner@fi.uba.ar>
2021-12-16 18:57:57 -0300

    chore: quito un constructor sin uso de Ladron

--- src/main/java/edu/fiuba/algo3/modelo/Ladron.java
+++ src/main/java/edu/fiuba/algo3/modelo/Ladron.java
@@ -2,30 +2,20 @@ package edu.fiuba.algo3.modelo;
 
 public class Ladron {
 
-    private final String sexo;
     private final String nombre;
     private final ObjetoRobado objetoRobado;
     private final DescripcionSospechoso descripcion;
-
-    public Ladron(ObjetoRobado objetoRobado, String sexo) {
-        this.nombre = "Desconocido";
-        this.sexo = sexo;
-        this.objetoRobado = objetoRobado;
-        this.descripcion = new DescripcionSospechoso(new Rasgo("Sexo", sexo));
-    }
 
     public Ladron(String nombre, DescripcionSospechoso descripcion, ObjetoRobado objetoRobado) {
         this.nombre = nombre;
         this.objetoRobado = objetoRobado;
         this.descripcion = descripcion;
-        this.sexo = "";
     }
 
     public Ladron(String nombre, DescripcionSospechoso descripcion) {
         this.nombre = nombre;
         this.objetoRobado = new Comun("Ninguno");
         this.descripcion = descripcion;
-        this.sexo = "";
     }
 
     public DescripcionSospechoso descripcion() {
