830264f7baa327f9bffddbcd95b5af647e392eff
Tomas Gruner <tgruner@fi.uba.ar>
2021-12-30 19:28:39 -0300

    feat: agrego funcionalidad de GUI

---
+++ src/main/java/edu/fiuba/algo3/controllers/VentanaPrincipalController.java
@@ -0,0 +1,15 @@
+package edu.fiuba.algo3.controllers;
+
+import edu.fiuba.algo3.App;
+
+public class VentanaPrincipalController {
+    private final App app;
+
+    public VentanaPrincipalController(App app) {
+        this.app = app;
+    }
+
+    public void computadora() {
+        System.out.println("hola");
+    }
+}
